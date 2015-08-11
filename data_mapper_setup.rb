c33207122b516813e6e960cbb1ee5ce6b4248e6f
Sivan Patel <sivan.patel@googlemail.com>
2015-08-11 12:03:27 +0100

    again mk2

--- data_mapper_setup.rb
+++ data_mapper_setup.rb
@@ -2,7 +2,7 @@ require 'data_mapper'
 
 env = ENV['RACK_ENV'] || 'development'
 
-DataMapper.setup(:default, ENV['postgres://localhost/dcb2oi413s43a4'] || "postgres://localhost/dcb2oi413s43a4")
+DataMapper.setup(:default, ENV["postgres://localhost/dcb2oi413s43a4"] || "postgres://localhost/bookmark_manager_#{env}")
 
 require './app/models/link'
 
