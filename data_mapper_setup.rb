25e404e6a27bfbeccff5abdf95e75ef5406b386b
Sivan Patel <sivan.patel@googlemail.com>
2015-08-11 13:07:21 +0100

    changes

--- data_mapper_setup.rb
+++ data_mapper_setup.rb
@@ -2,7 +2,7 @@ require 'data_mapper'
 
 env = ENV['RACK_ENV'] || 'development'
 
-DataMapper.setup(:default, "ENV["DATABASE_URL"]" || "postgres://remcqumtcdfptv:Fq5ILIJBUgn7EwUAHQkZkHzHDi@ec2-54-197-230-210.compute-1.amazonaws.com:5432/dcb2oi413s43a4")
+DataMapper.setup(:default, ENV["DATABASE_URL"] || "postgres://remcqumtcdfptv:Fq5ILIJBUgn7EwUAHQkZkHzHDi@ec2-54-197-230-210.compute-1.amazonaws.com:5432/dcb2oi413s43a4")
 
 require './app/models/link'
 
