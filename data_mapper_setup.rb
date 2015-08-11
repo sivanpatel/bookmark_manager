ede2cdd35d37830dd8f319044bbead24b08f51ca
Sivan Patel <sivan.patel@googlemail.com>
2015-08-11 11:49:29 +0100

    trying to get heroku working

--- data_mapper_setup.rb
+++ data_mapper_setup.rb
@@ -2,8 +2,9 @@ require 'data_mapper'
 
 env = ENV['RACK_ENV'] || 'development'
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+# DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
+DataMapper.setup(:default, dbtype://remcqumtcdfptv:Fq5ILIJBUgn7EwUAHQkZkHzHDi@ec2-54-197-230-210.compute-1.amazonaws.com:5432/dcb2oi413s43a4) 
 require './app/models/link'
 
 DataMapper.finalize
