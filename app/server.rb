3d1614892a62f11c02dd089d2b9f096f6c55f9c6
Sivan Patel <sivan.patel@googlemail.com>
2015-08-12 11:21:37 +0100

    Added test & implementation of password confirmation

--- app/server.rb
+++ app/server.rb
@@ -41,7 +41,8 @@ class Bookmark_Manager < Sinatra::Base
 
   post '/users' do
     user = User.create(email: params[:email],
-              password: params[:password])
+              password: params[:password],
+              password_confirmation: params[:password_confirmation])
     session[:user_id] = user.id
     redirect to('/links')
   end
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -8,12 +8,17 @@ feature 'User sign up' do
     expect(User.first.email).to eq('alice@example.com')
   end
 
-  def sign_up(email: 'alice@example.com', password: 'oranges!')
+  scenario 'requires a matchin confirmation password' do
+    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+  end
+
+  def sign_up(email: 'alice@example.com', password: '12345678', password_confirmation: '12345678')
     visit '/users/new'
     expect(page.status_code).to eq(200)
     fill_in :email,   with: email
     fill_in :password, with: password
+    fill_in :password_confirmation, with: password_confirmation
     click_button 'Sign up'
   end
-  
+
 end
